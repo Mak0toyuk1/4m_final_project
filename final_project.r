285ac9ddbb02893d7468f9390ccdbd860e24cbfe
Mak0t0yuk1 <xua32@mcmaster.ca>
2024-11-05 14:24:19 -0500

    Test 2

--- final_project.r
+++ final_project.r
@@ -19,6 +19,8 @@ plot(eigenvals,xlab="Principal Component",ylab="Eigenvalue",main="Eigenvalue vs.
 
 # Normality test for factor analysis. 
 
+# This is some text
+
 shapiro.test(diabetes_data$Pregnancies) # Not normal, p= 2.2e-16
 
 shapiro.test(diabetes_data$Glucose) # Not normal
@@ -32,3 +34,5 @@ shapiro.test(diabetes_data$Insulin) # Not normal
 shapiro.test(diabetes_data$BMI) # Not normal
 
 shapiro.test(diabetes_data$Age) # Not normal
+
+# This is some text
