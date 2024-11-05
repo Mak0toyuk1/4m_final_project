1c5b27a27012fafc937d971a583d775d41a7e4ff
Mak0t0yuk1 <xua32@mcmaster.ca>
2024-11-05 13:54:45 -0500

    Added some comments explaining how many principle components one should take.

--- final_project.r
+++ final_project.r
@@ -8,7 +8,7 @@ shapiro.test(diabetes_data$len)
 diabetes_pca<-prcomp(diabetes_data,scale=TRUE)
 diabetes_pca
 
-summary(diabetes_pca)
+summary(diabetes_pca) # 80 percent rules suggests that we should take 5 principle components.
 sum(diabetes_pca$sdev^2)
 
 eigenvals <- diabetes_pca$sdev^2
@@ -16,4 +16,4 @@ eigenvals
 
 cumsum(eigenvals)/sum(eigenvals) # same as last line in summary(heptathlon_pca2) "Cumulative Proportion"
 
-plot(eigenvals,xlab="Principal Component",ylab="Eigenvalue",main="Eigenvalue vs. Principal) Component",type ="l" )
+plot(eigenvals,xlab="Principal Component",ylab="Eigenvalue",main="Eigenvalue vs. Principal) Component",type ="l" ) #Eigenvalues suggest that we should take 3 principle components.
