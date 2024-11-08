e452c8710c324f1d47b6ece7acc6d5b2dc3a7dc7
rkazim <166972799+rkazim@users.noreply.github.com>
2024-11-08 13:38:43 -0500

    Update final_project.r

--- final_project.r
+++ final_project.r
@@ -3,10 +3,6 @@
 diabetes_data <-read.csv("/home/evo/Mcmaster/4m03/R code/diabetes_dataset.csv") # Tony's dataset
 diabetes_data <-read.csv("C:/Users/msafi/OneDrive/Documents/GitHub/4m_final_project/diabetes_dataset.csv") # Safi's dataset
 
-# Using the normal QQ-Plot to confirm that the dataset is not normally distributed
-qqnorm(diabetes_data[,1])
-qqline(diabetes_data[,1])
-
 library(ggplot2)
 library(dplyr)# Library for the Shapiro-Wilk test
 
@@ -41,6 +37,10 @@ shapiro.test(diabetes_data$Insulin) # Not normal
 shapiro.test(diabetes_data$BMI) # Not normal
 
 shapiro.test(diabetes_data$Age) # Not normal
+
+# Using the normal QQ-Plot to confirm that the dataset is not normally distributed
+qqnorm(diabetes_data[,1])
+qqline(diabetes_data[,1])
 
 # This is some text
 
