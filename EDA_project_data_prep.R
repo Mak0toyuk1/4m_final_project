2b3d4be9cdd5f3152f9ce9fbec2cf19b211ee6a1
Safi Khan <msafikhan@outlook.com>
2024-11-12 14:47:41 -0500

    On behalf of Stark Industries, I will show you.. my ass.

--- EDA_project_data_prep.R
+++ EDA_project_data_prep.R
@@ -3,74 +3,77 @@ library(dendextend)
 library(RColorBrewer)
 library(reshape2)
 library(ggthemes)
+#install.packages("ggthemes") will need to run if you get error message "Error in library(ggthemes) : there is no package called 'ggthemes'"
 library(GGally)
 library(hdrcde)
+#install.packages("hdrcde") will need to run if you get error message "Error in library(ggthemes) : there is no package called 'hdrcde'"
 library(KernSmooth)
 library(ggplot2)
 library(gridExtra)
 library(corrplot)
 library(wordcloud)
+#install.packages("wordcloud") will need to run if you get error message "Error in library(ggthemes) : there is no package called 'wordcloud'"
 library(wordcloud2)
+#install.packages("wordcloud2") will need to run if you get error message "Error in library(ggthemes) : there is no package called 'wordcloud2'"
 library(wesanderson)
-library(RColorBrewer)
+#install.packages("wesanderson") will need to run if you get error message "Error in library(ggthemes) : there is no package called 'wesanderson'"
 library(plotly)
-library(kernlab)#
+library(kernlab)
 library(vscc)
 library(caret)
 
+diabetes_data <-read.csv("C:/Users/msafi/OneDrive/Documents/GitHub/4m_final_project/diabetes_dataset.csv") # Safi's Path to the Data Set
 
-
-data("iris")
-df1=iris
+df=diabetes_data
 ## quick look
-head(df1)
-attach(df1)
-str(df1)
-dim(df1)
+head(df)
+attach(df)
+str(df)
+dim(df)
 
 
 #quick summary
-summary(df1)
+summary(df)
 # finds the count of missing values 
 sum(is.na(df))
 
 ###pair plot 
-ggpairs(df[,-c(5)], aes(colour=as.factor(Species), alpha=0.4),lower=list(continuous="points"),
+ggpairs(df[,-c(9)], aes(colour=as.factor(Outcome), alpha=0.4),lower=list(continuous="points"),
         axisLabels="none", switch="both")
 ### bar plot 
 
 # one variable
 
-ggplot(df,aes(x=Species,y=Sepal.Length,fill=factor(Species)))+
+ggplot(df,aes(x=Outcome,y=Glucose,fill=factor(Outcome)))+
   geom_bar(stat="identity")+theme_minimal()+
-  scale_fill_brewer(palette="RdPu",name="Species")+
+  scale_fill_brewer(palette="RdPu",name="Outcome")+
   theme_minimal()+ xlab("")+ylab("")
 # all variables
-gg <- melt(df,id="Species")   # df is your original table
-ggplot(gg, aes(x=variable, y=value, fill=factor(Species))) + 
+gg <- melt(df,id="Outcome")   # df is your original table
+ggplot(gg, aes(x=variable, y=value, fill=factor(Outcome))) + 
   stat_summary(fun.y=mean, geom="bar",position=position_dodge(1)) + 
   theme_minimal()+
-  scale_fill_brewer(palette="PuBuGn",name="Species")+
+  scale_fill_brewer(palette="PuBuGn",name="Outcome")+
   theme_minimal()+ xlab("")+ylab("")
 
 #### pie chart
 data <- df %>% 
-  group_by(Species) %>% 
+  group_by(Outcome) %>% 
   count() %>% 
   ungroup() %>% 
   mutate(per=`n`/sum(`n`)) %>% 
-  arrange(desc(Species))
+  arrange(desc(Outcome))
 data$label <- scales::percent(data$per)
 ggplot(data=data)+
-  geom_bar(aes(x="", y=per, fill=Species), stat="identity", width = 1)+
-  coord_polar("y", start=0)+scale_fill_brewer("Species")+
+  geom_bar(aes(x="", y=per, fill=Outcome), stat="identity", width = 1)+
+  coord_polar("y", start=0)+scale_fill_brewer("Outcome")+
   theme_void()+
   geom_text(aes(x=1, y = cumsum(per) - per/2, label=label)
   )
 
 ## correlation 
 
-data2<-df[,-c(5)]
+data2<-df[,-c(9)]
 corr1<-cor(data2)
 corrplot(corr1, method = "circle",bg = "white", 
          type="lower", order="hclust", tl.cex = 0.75, 
@@ -81,19 +84,19 @@ ggcorr(data2, label = TRUE, label_size = 3,
        label_round = 2, label_alpha = TRUE)
 
 ### grid  plots 
-ggparcoord(iris, columns=1:4, groupColumn="Species")
+ggparcoord(, columns=1:8, groupColumn="Outcome")
 
-a <- ggplot(iris, aes("Boxplot for all", Sepal.Width)) +
+a <- ggplot(diabetes_data, aes("Boxplot for all", Glucose)) +
   xlab("")  + geom_boxplot() +
   scale_x_discrete(breaks=NULL) 
-b <- ggplot(iris, aes(Species, Sepal.Width)) + 
+b <- ggplot(diabetes_data, aes(Outcome, Glucose)) + 
   geom_boxplot() +  xlab("")
 grid.arrange(a, b, nrow=1, widths=c(1,2))
 
-a <- ggplot(iris, aes("Boxplot for all", Petal.Width)) +
+a <- ggplot(diabetes_data, aes("Boxplot for all", Insulin)) +
   xlab("")  + geom_boxplot() +
   scale_x_discrete(breaks=NULL) 
-b <- ggplot(iris, aes(Species, Petal.Width)) + 
+b <- ggplot(diabetes_data, aes(Outcome, Insulin)) + 
   geom_boxplot() +  xlab("")
 grid.arrange(a, b, nrow=1, widths=c(1,2))
 
@@ -193,7 +196,7 @@ plot(coffeerun)
 data("iris")
 df=iris
 set.seed(2344)
-train.index <- createDataPartition(df$Species, p = .75, list = F)
+train.index <- createDataPartition(df$Outcome, p = .75, list = F)
 train <- df[ train.index,]
 test  <- df[-train.index,]
 # random  splitting/sampling
@@ -201,3 +204,4 @@ set.seed(2344)
 train_ind_raw <- sample(nrow(df), size = floor(0.75 * nrow(df)))
 train<- as.data.frame(df[train_ind_raw, ])
 test <- as.data.frame(df[-train_ind_raw, ])
+
